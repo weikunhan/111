f0ef2d1815cb1a6948ba7ff4fe492a1753b90a33
Han::Mark <weikunhan@g.ucla.edu>
2017-05-11 15:24:12 -0700

    Update problem1.R

--- Code/Assignment 3/problem1.R
+++ Code/Assignment 3/problem1.R
@@ -35,5 +35,5 @@ gcc_index <- which.max(cluster$csize)
 non_gcc_nodes <- (1:vcount(graph))[cluster$membership != gcc_index]
 gcc <- delete.vertices(graph, non_gcc_nodes)
 
-# Plot the GCC
+# Plot information
 plot(gcc)
